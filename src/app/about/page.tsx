647246f5c7532808742e1685bb2d295cc3df33fa
Shu Yaib <mdshuyaibislam5050@gmail.com>
2026-01-18 15:08:53 +0000

    ২০০ px করো (_for element img_)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -164,7 +164,7 @@ export default function AboutPage() {
             {/* Column 1: Logo and Description */}
             <div className="space-y-4">
               <Link href="/">
-                <Image src="https://raw.githubusercontent.com/shuyaib105/syllabuserbaire/refs/heads/main/ei_1766508088751-removebg-preview.png" alt="Footer Logo" width={80} height={80} quality={100} />
+                <Image src="https://raw.githubusercontent.com/shuyaib105/syllabuserbaire/refs/heads/main/ei_1766508088751-removebg-preview.png" alt="Footer Logo" width={200} height={200} quality={100} />
               </Link>
               <p className="text-gray-600 font-tiro-bangla text-sm max-w-xs">
                 {heroData.subtitle}
@@ -211,5 +211,3 @@ export default function AboutPage() {
     </div>
   );
 }
-
-    
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
             {/* Column 1: Logo and Description */}
             <div className="space-y-4">
               <Link href="/">
-                <Image src="https://raw.githubusercontent.com/shuyaib105/syllabuserbaire/refs/heads/main/ei_1766508088751-removebg-preview.png" alt="Footer Logo" width={80} height={80} quality={100} />
+                <Image src="https://raw.githubusercontent.com/shuyaib105/syllabuserbaire/refs/heads/main/ei_1766508088751-removebg-preview.png" alt="Footer Logo" width={200} height={200} quality={100} />
               </Link>
               <p className="text-gray-600 font-tiro-bangla text-sm max-w-xs">
                 {heroData.subtitle}
@@ -186,5 +186,3 @@ export default function Home() {
     </div>
   );
 }
-
-    
